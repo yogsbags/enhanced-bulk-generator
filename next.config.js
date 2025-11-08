a4a7e6d1630a2ad4247317e85aa2738616170f4b
Yogesh Bagle <yogs87@Yogeshs-MacBook-Pro.local>
2025-11-09 00:28:53 +0530

    Add outputFileTracingIncludes to bundle backend files with serverless functions

--- next.config.js
+++ next.config.js
@@ -9,6 +9,29 @@ const nextConfig = {
   experimental: {
     // Externalize server packages for serverless functions
     serverComponentsExternalPackages: ['child_process'],
+
+    // Include backend workflow files in serverless function bundles
+    // This tells Next.js to copy these files when deploying API routes
+    outputFileTracingIncludes: {
+      '/api/workflow/execute': [
+        '../main.js',
+        '../core/**/*',
+        '../research/**/*',
+        '../content/**/*',
+        '../data/**/*',
+        '../package.json',
+        '../node_modules/**/*',
+      ],
+      '/api/workflow/stage': [
+        '../main.js',
+        '../core/**/*',
+        '../research/**/*',
+        '../content/**/*',
+        '../data/**/*',
+        '../package.json',
+        '../node_modules/**/*',
+      ],
+    },
   },
 }
 
